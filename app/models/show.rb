7f85c62f468098c7c67252bc3113eb63f015548d
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-14 22:39:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,8 +12,8 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     show = Show.minimum(:rating)
   end
-def self.least_popular_show
-  show = Show.lowest_rating
-  Show.find_by(rating: show)
-end
+  def self.least_popular_show
+    show = Show.lowest_rating
+    Show.find_by(rating: show)
+  end
 end
